e4f1e15279ae7ceb2a2090c1901e1d9c19898760
danishsshaikh <danishsshaikh@gmail.com>
2020-08-12 17:45:10 +0530

    added eventListener

---
+++ index.js
@@ -0,0 +1,10 @@
+var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumsButtons; i++){
+	document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+		alert("welp, got clicked again");
+	});
+
+}
+
+
