1e5df65c6e760ff696633f0d5df3c5798a27eea5
danishsshaikh <danishsshaikh@gmail.com>
2020-08-13 17:11:02 +0530

    adding images

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i<numberOfDrumsButtons; i++){
 	document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-		alert("welp, got clicked again");
+		
 	});
 
 }
